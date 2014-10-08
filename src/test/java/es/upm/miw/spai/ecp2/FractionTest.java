ddb11adec2950d47b1f910be5f8d5d73aea8edf9
nataliaznar <nataliaznar@hotmail.com>
2014-10-08 20:10:34 +0200

    Test reducir fraccion hecho. Tiempo empleado: 20 minutos

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -26,5 +26,19 @@ public class FractionTest {
         fr = new Fraction (12,4);
         assertEquals(3, fr.decimal(), 10e-5);
     }
+    
+    @Test
+    public void testReducirFraccion() {
+        fr = new Fraction (12,4);
+        Fraction[] f1 = new Fraction[2];
+    	f1[0] = new Fraction(2,4);
+    	f1[1] = new Fraction(1,4);
+    	Fraction[] f = new Fraction[2];
+    	f[0] = new Fraction(1,2);
+    	f[1] = new Fraction(1,4);
+        assertArrayEquals(f1, fr.reducirFraccion(f));
+    }
+    
+    
 
 }
